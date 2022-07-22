583b4566633cbca6139d5c8af6a03da7e8cf14b5
Angelo Perez <angellloperezzz@gmail.com>
2022-07-22 13:53:11 -0700

    initial commit

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ let Ebuttoncounter = 0
 
 Ebutton.onclick = function() {
     const background = document.querySelector('body')
-    
     background.style["background-image"] = "url('kinotabi.gif')"
     const buttonMessage = document.querySelector('.start-message')
     buttonMessage.textContent = "Welcome to Initial D, Enter your username to continue"
