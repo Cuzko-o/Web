a1ae8a6fff815af6620df0c488f4955145f1c05e
Angelo Perez <angellloperezzz@gmail.com>
2022-07-22 13:43:11 -0700

    initial commit

--- app.js
+++ app.js
@@ -2,14 +2,20 @@
 console.log("document is ", document)
 const Ebutton = document.querySelector('.enter-button')
 
+
+
 let Ebuttoncounter = 0
+
 Ebutton.onclick = function() {
+    const background = document.querySelector('body')
+    
+    background.style["background-image"] = "url('kinotabi.gif')"
     const buttonMessage = document.querySelector('.start-message')
     buttonMessage.textContent = "Welcome to Initial D, Enter your username to continue"
     buttonMessage.style["color"] = "white"
     Ebuttoncounter += 1
 
-    const background = document.querySelector('body')
+    
     if ( Ebuttoncounter >= 2) {
         background.style["background-image"] = "url('insert.gif')"
         const buttonMessage = document.querySelector('.start-message')
